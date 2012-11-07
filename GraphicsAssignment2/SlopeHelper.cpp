974daf1879c7e0f1f20e475b8275afe3952675af
Stewart Taylor <stewart@taylore.net>
2012-11-07 10:20:33 +0000

    Refactored SlopeHelper

--- GraphicsAssignment2/SlopeHelper.cpp
+++ GraphicsAssignment2/SlopeHelper.cpp
@@ -1,13 +1,18 @@
-
+/*		Slope Helper
+ *	AUTHOR: STEWART TAYLOR
+ *------------------------------------
+ * This class is used to provide functions that will determine the slope of a surface
+ *
+ *
+ * Last Updated: 05/11/2012
+*/
 
 #include "SlopeHelper.h"
-
 #include <math.h>
 
 SlopeHelper::SlopeHelper(void)
 {
 }
-
 
 SlopeHelper::~SlopeHelper(void)
 {
@@ -16,149 +21,147 @@ SlopeHelper::~SlopeHelper(void)
 
 float  SlopeHelper::getSlopeValue(int x , int y , GLfloat landsc[65][65])
 {
-	        float topLeft = getTopLeftSlope(x, y ,  landsc);
-            float topMid = getTopMiddleSlope(x, y, landsc);
-            float topRight = getTopRightSlope(x, y,  landsc);
+	size = 65;
 
-            float midLeft = getMiddleLeftSlope(x, y,  landsc);
-            float midRight = getMiddleRightSlope(x, y,  landsc);
+	float topLeft = getTopLeftSlope(x, y ,  landsc);
+	float topMid = getTopMiddleSlope(x, y, landsc);
+	float topRight = getTopRightSlope(x, y,  landsc);
 
-            float botLeft = getBottomLeftSlope(x, y,  landsc);
-            float botMid = getBottomMiddleSlope(x, y,  landsc);
-            float botRight = getBottomRightSlope(x, y,  landsc);
+	float midLeft = getMiddleLeftSlope(x, y,  landsc);
+	float midRight = getMiddleRightSlope(x, y,  landsc);
 
-
-            float high = 0;
-
-            if (topLeft >= high) { high = topLeft; }
-            if (topMid >= high) { high = topMid; }
-            if (topRight >= high) { high = topRight; }
-            if (midLeft >= high) { high = midLeft; }
-            if (midRight >= high) { high = midRight; }
-            if (botLeft >= high) { high = botLeft; }
-            if (botMid >= high) { high = botMid; }
-            if (botRight >= high) { high = botRight; }
+	float botLeft = getBottomLeftSlope(x, y,  landsc);
+	float botMid = getBottomMiddleSlope(x, y,  landsc);
+	float botRight = getBottomRightSlope(x, y,  landsc);
 
 
-            float total = topLeft + topRight + topMid + midLeft + midRight + botLeft + botMid + botRight;
+	float high = 0;
 
-            // float value = total / 8;
-            float value = high;
+	if (topLeft >= high) { high = topLeft; }
+	if (topMid >= high) { high = topMid; }
+	if (topRight >= high) { high = topRight; }
+	if (midLeft >= high) { high = midLeft; }
+	if (midRight >= high) { high = midRight; }
+	if (botLeft >= high) { high = botLeft; }
+	if (botMid >= high) { high = botMid; }
+	if (botRight >= high) { high = botRight; }
 
-            return value;
+	float total = topLeft + topRight + topMid + midLeft + midRight + botLeft + botMid + botRight;
+
+    // float value = total / 8;
+	float value = high;
+
+	return value;
 }
 
 
-  GLfloat SlopeHelper::getGradientValue(GLfloat x, GLfloat y)
-        {
-            GLfloat gradient = 0;
+GLfloat SlopeHelper::getGradientValue(GLfloat x, GLfloat y)
+{
+	GLfloat gradient = 0;
 
-            if ((x > 0) && (y > 0))
-            {
-                gradient = x - y;
-                gradient = abs(gradient);
-            }
+	if ((x > 0) && (y > 0))
+	{
+		gradient = x - y;
+		gradient = abs(gradient);
+	}
 
-            return gradient;
-
-        }
-
+	return gradient;
+}
 
 
-         GLfloat SlopeHelper::getTopLeftSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
 
-            if ((x > 0) && (y > 0))
-            {
-                gradient = getGradientValue(landsc[x - 1][ y - 1], landsc[x][ y]);
-            }
+GLfloat SlopeHelper::getTopLeftSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-            return gradient;
-        }
+    if ((x > 0) && (y > 0))
+    {
+		gradient = getGradientValue(landsc[x - 1][ y - 1], landsc[x][ y]);
+	}
 
-         GLfloat SlopeHelper::getTopMiddleSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
+	return gradient;
+}
 
-            if (y > 0)
-            {
-                gradient = getGradientValue(landsc[x][ y - 1], landsc[x][ y]);
-            }
+GLfloat SlopeHelper::getTopMiddleSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-            return gradient;
-        }
+	if (y > 0)
+	{
+		gradient = getGradientValue(landsc[x][ y - 1], landsc[x][ y]);
+	}
 
-         GLfloat SlopeHelper::getTopRightSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
+	return gradient;
+}
 
-            if ((x < 64 - 1) && (y > 0))
-            {
+GLfloat SlopeHelper::getTopRightSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
+
+	if ((x < size - 1) && (y > 0))
+	{
                 gradient = getGradientValue(landsc[x + 1][ y - 1], landsc[x][ y]);
-            }
+	}
 
-            return gradient;
-        }
+	return gradient;
+}
 
-         GLfloat SlopeHelper::getMiddleLeftSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
+GLfloat SlopeHelper::getMiddleLeftSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-            if (x > 0)
-            {
-                gradient = getGradientValue(landsc[x - 1][ y], landsc[x][ y]);
-            }
+	if (x > 0)
+	{
+		gradient = getGradientValue(landsc[x - 1][ y], landsc[x][ y]);
+	}
 
-            return gradient;
+	return gradient;
+}
 
-        }
+GLfloat SlopeHelper::getMiddleRightSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-         GLfloat SlopeHelper::getMiddleRightSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
+    if (x < size - 1)
+    {
+		gradient = getGradientValue(landsc[x + 1][ y], landsc[x][ y]);
+	}
 
+	return gradient;
+}
 
-            if (x < 64 - 1)
-            {
-                gradient = getGradientValue(landsc[x + 1][ y], landsc[x][ y]);
-            }
+GLfloat SlopeHelper::getBottomLeftSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-            return gradient;
-        }
+	if ((x > 0) && (y < size - 1))
+	{
+		gradient = getGradientValue(landsc[x - 1][ y + 1], landsc[x][ y]);
+	}
 
-         GLfloat SlopeHelper::getBottomLeftSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
+	return gradient;
+}
 
-            if ((x > 0) && (y < 64 - 1))
-            {
-                gradient = getGradientValue(landsc[x - 1][ y + 1], landsc[x][ y]);
-            }
+GLfloat SlopeHelper::getBottomMiddleSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-            return gradient;
-        }
+	if (y < size - 1)
+	{
+		gradient = getGradientValue(landsc[x][ y + 1], landsc[x][ y]);
+	}
 
-         GLfloat SlopeHelper::getBottomMiddleSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
+	return gradient;
+}
 
-            if (y < 64 - 1)
-            {
-                gradient = getGradientValue(landsc[x][ y + 1], landsc[x][ y]);
-            }
+GLfloat SlopeHelper::getBottomRightSlope(int x, int y , GLfloat landsc[65][65])
+{
+	GLfloat gradient = 0;
 
-            return gradient;
-        }
+	if ((x < size - 1) && (y < size - 1))
+	{
+		gradient = getGradientValue(landsc[x + 1][ y + 1], landsc[x][ y]);
+	}
 
-         GLfloat SlopeHelper::getBottomRightSlope(int x, int y , GLfloat landsc[65][65])
-        {
-            GLfloat gradient = 0;
-
-            if ((x < 64 - 1) && (y < 64 - 1))
-            {
-                gradient = getGradientValue(landsc[x + 1][ y + 1], landsc[x][ y]);
-            }
-
-            return gradient;
-        }
+	return gradient;
+}
--- GraphicsAssignment2/SlopeHelper.h
+++ GraphicsAssignment2/SlopeHelper.h
@@ -7,14 +7,16 @@ public:
 	SlopeHelper(void);
 	~SlopeHelper(void);
 	static	float getSlopeValue(int x , int y, GLfloat landsc[65][65]);
+private:
+	static int size;
 	static GLfloat getGradientValue(GLfloat x, GLfloat y);
 	static GLfloat getTopLeftSlope(int x, int y, GLfloat landsc[65][65]);
 	static GLfloat getTopMiddleSlope(int x, int y, GLfloat landsc[65][65]);
 	static GLfloat getTopRightSlope(int x, int y, GLfloat landsc[65][65]);
 	static GLfloat getMiddleLeftSlope(int x, int y, GLfloat landsc[65][65]);
-static	GLfloat getMiddleRightSlope(int x, int y, GLfloat landsc[65][65]);
-static	GLfloat getBottomLeftSlope(int x, int y, GLfloat landsc[65][65]);
-static	GLfloat getBottomMiddleSlope(int x, int y, GLfloat landsc[65][65]);
-static	GLfloat getBottomRightSlope(int x, int y , GLfloat landsc[65][65]);
+	static	GLfloat getMiddleRightSlope(int x, int y, GLfloat landsc[65][65]);
+	static	GLfloat getBottomLeftSlope(int x, int y, GLfloat landsc[65][65]);
+	static	GLfloat getBottomMiddleSlope(int x, int y, GLfloat landsc[65][65]);
+	static	GLfloat getBottomRightSlope(int x, int y , GLfloat landsc[65][65]);
 };
 
